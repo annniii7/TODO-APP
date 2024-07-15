8ea3748887113f048f315e2401a310ec8a1dd18b
anirudh <anirudhsharma4921@gmail.com>
2024-07-15 15:58:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container bg-slate-600">hello</div>
+    </>
+  )
+}
+
+export default App
